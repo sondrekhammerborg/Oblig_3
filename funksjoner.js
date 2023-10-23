e24b86920c35bb1bcb80fcdae0794ec614242993
sondrekhammerborg <sondrekh@hiof.no>
2023-10-23 17:30:55 +0200

    Revert "Initial commit"
    This reverts commit c1750d1d831eae197239997eacd77c2c0dbbd481.

--- funksjoner.js
+++
@@ -1,8 +0,0 @@
-function isLeapYear(year) {
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-      return true;
-    }
-    else return false;
-  }
-
-    module.exports = {isLeapYear : isLeapYear}
